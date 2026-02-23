2781af6cd8cfd8dd93a80347f1764cd40ef1bab2
Miru-Anim <Dakkuczeb@proton.me>
2026-02-23 17:35:32 +0000

    ww

---
+++ Php/sprphp.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <php?
+    $liczba=4;
+
+    if($liczba % == 2)
+        {
+            echo "liczba $liczba z dzielenia jest parzysta";
+        }
+        else 
+        {
+            ehco "liczba $liczba nie jest parzysta";
+        }
+
+    ?>
+</body>
+</html>
